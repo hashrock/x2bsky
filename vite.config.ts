3dc584aead83554b10fcadf8d93345ea2eafb5bd
hashrock <hashrock@users.noreply.github.com>
2024-11-26 00:02:34 +0900

    add html entrypoint

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,11 @@ export default defineConfig({
   build: {
     outDir: "dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
+    },
   },
   server: {
     port: 5173,
